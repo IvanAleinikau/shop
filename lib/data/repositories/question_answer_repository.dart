3420092708fd1800c32b541a3a118e5bc012e3ef
IvanAleinikau <aleynikov.05@yandex.ru>
2021-09-08 15:55:02 +0300

    fix comments

--- lib/data/repositories/question_answer_repository.dart
+++ lib/data/repositories/question_answer_repository.dart
@@ -5,6 +5,8 @@ import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 
 class QuestionAnswerRepository {
+  late Database database;
+
   Future<Database> initializeDB() async {
     String path = await getDatabasesPath();
     return openDatabase(
@@ -20,20 +22,20 @@ class QuestionAnswerRepository {
 
   Future<int> insertQuestionAnswer(QuestionAnswer qa) async {
     int result = 0;
-    final Database db = await initializeDB();
-    result = await db.insert('question_answer', qa.toJson());
+    database = await initializeDB();
+    result = await database.insert('question_answer', qa.toJson());
     return result;
   }
 
   Future<List<QuestionAnswer>> retrieveQuestionAnswer() async {
-    final Database db = await initializeDB();
-    final List<Map<String, Object?>> queryResult = await db.query('question_answer');
+    database = await initializeDB();
+    final List<Map<String, Object?>> queryResult = await database.query('question_answer');
     return queryResult.map((e) => QuestionAnswer.fromJson(e)).toList();
   }
 
   Future<void> deleteQuestionAnswer(int id) async {
-    final db = await initializeDB();
-    await db.delete(
+    database = await initializeDB();
+    await database.delete(
       'question_answer',
       where: 'id = ?',
       whereArgs: [id],
--- lib/data/repositories/saved_news_repository.dart
+++ lib/data/repositories/saved_news_repository.dart
@@ -5,6 +5,8 @@ import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 
 class SavedNewsRepository {
+  late Database database;
+
   Future<Database> initializeDB() async {
     String path = await getDatabasesPath();
     return openDatabase(
@@ -20,20 +22,20 @@ class SavedNewsRepository {
   
   Future<int> insertSavedNews(SavedNews savedNews) async {
     int result = 0;
-    final Database db = await initializeDB();
-    result = await db.insert('news', savedNews.toJson());
+    database = await initializeDB();
+    result = await database.insert('news', savedNews.toJson());
     return result;
   }
 
   Future<List<SavedNews>> retrieveSavedNews() async {
-    final Database db = await initializeDB();
-    final List<Map<String, Object?>> queryResult = await db.query('news');
+    database = await initializeDB();
+    final List<Map<String, Object?>> queryResult = await database.query('news');
     return queryResult.map((e) => SavedNews.fromJson(e)).toList();
   }
 
   Future<void> deleteSavedNews(int id) async {
-    final db = await initializeDB();
-    await db.delete(
+    database = await initializeDB();
+    await database.delete(
       'news',
       where: 'id = ?',
       whereArgs: [id],
